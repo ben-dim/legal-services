a5567c0d537b7ba984f48f0f6ada52f55a7e5729
ben-dim <83243798+ben-dim@users.noreply.github.com>
2022-02-10 14:56:21 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(function () {
+
+	$('.slider__inner, .news__slider-body').slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false,
+		dots: true,
+	 });
+
+$('.header__burger').click(function(event){
+	$('.header__burger,.menu ul').toggleClass('active');
+});
+});
+
+
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
+
+} else {
+	document.body.classList.add('_pc');
+}
